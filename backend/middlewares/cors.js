41ea9d24dd7da4bd84692b244d6e8bd9426861fe
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-07-25 21:28:01 +0500

    Url for API fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,5 +1,5 @@
 const { NODE_ENV, ALLOWED_CORS_PRODUCTION } = process.env;
-const ALLOWED_CORS = NODE_ENV === 'production' ? ALLOWED_CORS_PRODUCTION.split(', ') : ['https://localhost:3000', 'http://localhost:3000', 'localhost:3000', 'http://genossek.mesto.nomoredomains.xyz/', 'https://genossek.mesto.nomoredomains.xyz/'];
+const ALLOWED_CORS = NODE_ENV === 'production' ? ALLOWED_CORS_PRODUCTION.split(', ') : ['https://localhost:3000', 'http://localhost:3000', 'localhost:3000', 'http://genossek.mesto.nomoredomains.xyz', 'https://genossek.mesto.nomoredomains.xyz'];
 
 const CORS_OPTIONS = {
   origin: ALLOWED_CORS,
